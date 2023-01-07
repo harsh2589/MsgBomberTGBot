b3379bb5436b3180d08230ef70a9c2f45f99fc38
harsh2589 <95346928+harsh2589@users.noreply.github.com>
2023-01-07 12:33:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
     AUTH_USERS = [2037257558]
     # Add numbers below who shouldn't be bombed ever
-    NO_BOMB_NUMS = []
+    NO_BOMB_NUMS = [7814466788]
     # Add userids below of users who should have sudo authority over bot, i.e., have no bombing limits
     GOD_USERS = [2037257558]
